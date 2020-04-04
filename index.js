ada56deaf05646aefac0a3d9013e02d99a6242a1
korrio <iamnotkorr@gmail.com>
2020-04-04 11:51:20 +0700

    :fire: firelab

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-const express = require("express");
-const app = express();
+var jsonServer = require('json-server');
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+var port = process.env.PORT || 5000;
 
-const port = 5000;
-
-// Body parser
-app.use(express.urlencoded({ extended: false }));
-
-// Home route
-app.get("/", (req, res) => {
-  res.send("Welcome to a basic express App");
-});
-
-// Mock API
-app.get("/users", (req, res) => {
-  res.json([
-    { name: "William", location: "Abu Dhabi" },
-    { name: "Chris", location: "Vegas" }
-  ]);
-});
-
-app.post("/user", (req, res) => {
-  const { name, location } = req.body;
-
-  res.send({ status: "User created", name, location });
-});
-
-// Listen on port 5000
-app.listen(port, () => {
-  console.log(`Server is booming on port 5000
-Visit http://localhost:5000`);
+server.use(middlewares);
+server.use(router);
+server.listen(port, function() {
+  console.log('JSON Server is running on http://localhost:' + port);
 });
